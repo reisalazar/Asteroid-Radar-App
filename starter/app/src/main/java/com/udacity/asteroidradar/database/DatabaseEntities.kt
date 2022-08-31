a1bd9ac7aae353299729ea4c0b722493d2387a4b
reisalazar <rrsb45@gmail.com>
2022-08-31 17:35:32 -0300

    Database Entities

--- starter/app/src/main/java/com/udacity/asteroidradar/database/DatabaseEntities.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/database/DatabaseEntities.kt
@@ -1,17 +1,26 @@
 package com.udacity.asteroidradar.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "asteroids_table")
 data class AsteroidEntity constructor(
-    @PrimaryKey
-    val id: Long,
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0L,
+    @ColumnInfo(name = "code_name")
     val codename: String,
+    @ColumnInfo(name = "close_approach_date")
     val closeApproachDate: String,
+    @ColumnInfo(name = "absolute_magnitude")
     val absoluteMagnitude: Double,
+    @ColumnInfo(name = "estimated_diameter")
     val estimatedDiameter: Double,
+    @ColumnInfo(name = "relative_velocity")
     val relativeVelocity: Double,
+    @ColumnInfo(name = "distance_from_earth")
     val distanceFromEarth: Double,
-    val isPotentiallyHazardous: Boolean)
+    @ColumnInfo(name = "is_potentially_hazardous")
+    val isPotentiallyHazardous: Boolean
+)
